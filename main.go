af7df817dbe4cd16bee90be52a7d43d855d1b28d
Andrew Ellison <71777978+arbezy@users.noreply.github.com>
2026-01-27 19:35:12 +0000

    feat: init rating tree endpoint

--- main.go
+++ main.go
@@ -11,21 +11,41 @@ type OptionalInt struct {
 }
 
 type MovieReview struct {
-	ReviewID  int         `json:"review_id"`
-	ParentId  OptionalInt `json:"parent_id"`
-	MovieName string      `json:"movie_name"`
-	Rating    int         `json:"rating"`
+	ReviewID  int    `json:"review_id"`
+	MovieName string `json:"movie_name"`
+	Rating    int    `json:"rating"`
+}
+
+type RatingTreeNode struct {
+	Review MovieReview     `json:"value"`
+	Left   *RatingTreeNode `json:"left"`
+	Right  *RatingTreeNode `json:"right"`
 }
 
 func getReview(w http.ResponseWriter, r *http.Request) {
-	var rev = MovieReview{0, OptionalInt{}, "testmovie", 5}
+	var rev = MovieReview{0, "testmovie", 5}
 
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(rev)
 }
 
+func getRatingTree(w http.ResponseWriter, r *http.Request) {
+	var parentRev = MovieReview{0, "testmovie1", 5}
+	var childRev0 = MovieReview{1, "testmovie2", 5}
+	var childRev1 = MovieReview{2, "testmovie3", 5}
+
+	var node0 = RatingTreeNode{childRev0, nil, nil}
+	var node1 = RatingTreeNode{childRev1, nil, nil}
+
+	tree := RatingTreeNode{Review: parentRev, Left: &node0, Right: &node1}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(tree)
+}
+
 func handleRequests() {
 	http.Handle("/review", http.HandlerFunc(getReview))
+	http.Handle("/tree", http.HandlerFunc(getRatingTree))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
